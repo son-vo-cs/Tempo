c2626a709eae70f846c5e9948e398a0f0e91da8d
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2019-10-11 14:00:06 -0700

    Add files via upload

---
+++ Assets/Assets/Activator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    Color old;
+    int scoreChange = 100;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Start()
+    {
+        old = sr.color;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            StartCoroutine(Pressed());
+        }
+        if (Input.GetKeyDown(key) && active)
+        {
+            Destroy(note);
+            AddScore();
+            active = false;
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active = true;
+        if (col.gameObject.tag == "Note")
+            note = col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        active = false;
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + scoreChange);
+    }
+
+    IEnumerator Pressed()
+    {
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
+    }
+}
---
+++ Assets/Assets/PPText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPText : MonoBehaviour
+{
+
+    public string name;
+
+    
+
+    void Update()
+    {
+        GetComponent<Text>().text = PlayerPrefs.GetInt(name) + "";
+    }
+}

1d1f64b70f37341ac7860f700735c56bf222a3b2
Muhammed Hassan Mahmood <43229504+Heskpar@users.noreply.github.com>
2019-10-14 16:41:33 -0700

    read from file system added
    Added a capability to read notes from a file and spawn them as notes.

---
+++ Assets/LADevNoteScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System.Threading;
+
+public class LADevNoteScript : MonoBehaviour
+{
+    //static string path = ;
+    StreamReader noteStream = new StreamReader("Assets/LADevoteeNotes.txt");
+    //public GameObject n;
+    public GameObject snote;
+    public GameObject dnote;
+    public GameObject fnote;
+    public GameObject jnote;
+    public GameObject knote;
+    public GameObject lnote;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        snote = GameObject.Find("S Note");
+        dnote = GameObject.Find("D Note");
+        fnote = GameObject.Find("F Note");
+        jnote = GameObject.Find("J Note");
+        knote = GameObject.Find("K Note");
+        lnote = GameObject.Find("L Note");
+        */
+
+        StartCoroutine(noteCoroutine());
+    }
+
+    IEnumerator noteCoroutine()
+    {
+        while (!noteStream.EndOfStream)
+        {       
+            string inp_ln = noteStream.ReadLine();
+            char[] noteArr = inp_ln.ToCharArray();
+
+            foreach (char c in noteArr)
+            {
+
+                switch (c)
+                {
+                    case ';':
+                        yield return new WaitForSeconds(0.3f);
+                        
+                        break;
+                    case 'S':
+                        Instantiate(snote, new Vector3(-5, 6, 0), Quaternion.identity);
+                        //n = (Note)snote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                    case 'D':
+                        Instantiate(dnote, new Vector3(-3, 6, 0), Quaternion.identity);
+                        //n = (Note)dnote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                    case 'F':
+                        Instantiate(fnote, new Vector3(-1, 6, 0), Quaternion.identity);
+                        //n = (Note)fnote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                    case 'J':
+                        Instantiate(jnote, new Vector3(1, 6, 0), Quaternion.identity);
+                        //n = (Note)jnote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                    case 'K':
+                        Instantiate(knote, new Vector3(3, 6, 0), Quaternion.identity);
+                        //n = (Note)knote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                    case 'L':
+                        Instantiate(lnote, new Vector3(5, 6, 0), Quaternion.identity);
+                        //n = (Note)snote.GetComponent(typeof(Note));
+                        //n.setSpeed((float)-3);
+                        break;
+                        /*default:
+                            note = GameObject.Find(c + " Note");
+                            Instantiate(note, transform.position, Quaternion.identity);
+                            Note n = (Note)note.GetComponent(typeof(Note));
+                            n.setSpeed((float)-3);
+                            break; */
+                }
+
+                
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
--- Assets/Note.cs
+++ Assets/Note.cs
@@ -11,6 +11,10 @@ public class Note : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        speed = 0;
+    }
+    void Create()
+    {
 
     }
 
@@ -18,12 +22,16 @@ public class Note : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector2(0, -3);
+        rb.velocity = new Vector2(0, -5);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void setSpeed(float s)
+    {
+        speed = s;
     }
 }

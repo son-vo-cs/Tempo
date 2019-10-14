da452411ee645b63b4d37fe892b7e486f3f15774
Muhammed Hassan Mahmood <43229504+Heskpar@users.noreply.github.com>
2019-10-13 17:33:22 -0700

    Lanes and createmode added
    Added score mechanic. Added createmode. Added song to background. New scene, ladevotee has background. We will use this scene to hold the beats specific to this song.

---
+++ Assets/Activator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    public GameObject n;
+    GameObject note;
+    Color old;
+    int scoreChange = 10;
+
+    public bool createmode;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Start()
+    {
+        old = sr.color;
+    }
+
+
+    void Update()
+    {
+        if (createmode) 
+        {
+            if(Input.GetKeyDown(key))
+            {
+                Instantiate(n, transform.position, Quaternion.identity);
+            }
+        }
+        else { 
+            if (Input.GetKeyDown(key))
+            {
+                StartCoroutine(Pressed());
+            }
+            if (Input.GetKeyDown(key) && active)
+            {
+                Destroy(note);
+                AddScore();
+                active = false;
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active = true;
+        if (col.gameObject.tag == "Note")
+            note = col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        active = false;
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + scoreChange);
+    }
+
+    IEnumerator Pressed()
+    {
+        sr.color = old * 0.75f;
+        yield return new WaitForSeconds(0.1f);
+        sr.color = old;
+    }
+}
---
+++ Assets/PPText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPText : MonoBehaviour
+{
+
+    public string name;
+
+    void Awake()
+    {
+        PlayerPrefs.SetInt(name, 0);
+    }
+
+    void Update()
+    {
+        GetComponent<Text>().text = string.Format("SCORE\n{0:#00000}", PlayerPrefs.GetInt(name));
+    }
+}
